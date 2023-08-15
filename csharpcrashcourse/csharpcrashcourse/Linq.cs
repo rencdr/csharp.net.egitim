2ed9944a66abdf85262723118c254c3b40479f70
Erencan Demir <demirerencan@outlook.com>
2023-08-15 16:03:52 +0300

    linq eklendi

---
+++ csharpcrashcourse/csharpcrashcourse/Linq.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//class Ogrenci
+//{
+//    public string OgrenciAdi { get; set; }
+//    public string OgrenciYasi { get; set; }
+//    public string OgrenciSinifi { get; set; }
+//    public string OgrenciNotu { get; set; }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        List<Ogrenci> ogrenciler = new List<Ogrenci>
+//        {
+//            new Ogrenci { OgrenciAdi = "Ahmet", OgrenciYasi = "20", OgrenciSinifi = "11A", OgrenciNotu = "85" },
+//            new Ogrenci { OgrenciAdi = "Ayşe", OgrenciYasi = "19", OgrenciSinifi = "10B", OgrenciNotu = "92" },
+//            new Ogrenci { OgrenciAdi = "Mehmet", OgrenciYasi = "21", OgrenciSinifi = "12C", OgrenciNotu = "78" },
+//            // Diğer öğrencileri burada ekleyebilirsiniz.
+//        };
+
+//        // LINQ sorguları örnekleri
+//        var yuksekNotAlanlar = ogrenciler.Where(o => int.Parse(o.OgrenciNotu) >= 85);
+//        var sinifListesi = ogrenciler.Select(o => o.OgrenciSinifi).Distinct();
+//        var adiAhmetOlanlar = ogrenciler.Where(o => o.OgrenciAdi == "Ahmet");
+
+//        // Örnek sorguları ekrana yazdıralım
+//        Console.WriteLine("Yüksek Not Alan Öğrenciler:");
+//        foreach (var ogrenci in yuksekNotAlanlar)
+//        {
+//            Console.WriteLine(ogrenci.OgrenciAdi);
+//        }
+
+//        Console.WriteLine("Farklı Sınıflar:");
+//        foreach (var sinif in sinifListesi)
+//        {
+//            Console.WriteLine(sinif);
+//        }
+
+//        Console.WriteLine("Adı Ahmet Olan Öğrenciler:");
+//        foreach (var ogrenci in adiAhmetOlanlar)
+//        {
+//            Console.WriteLine(ogrenci.OgrenciAdi);
+//        }
+//    }
+//}
--- csharpcrashcourse/csharpcrashcourse/NesneyonelimliOOP.cs
+++ csharpcrashcourse/csharpcrashcourse/NesneyonelimliOOP.cs
@@ -1,118 +1,118 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace csharpcrashcourse
-//{
-//    internal class NesneyonelimliOOP
-//    {
+namespace csharpcrashcourse
+{
+    internal class NesneyonelimliOOP
+    {
 
-//        //CLASS:Sınıflar, OOP'de nesnelerin özelliklerini (alanlar) ve davranışlarını (metotlar) tanımlayan yapıdır. Örneğin, "Araba" sınıfı, renk ve hız gibi özelliklerle "HareketEt" gibi metotları içerebilir.
-//        //class Car
-//        //{
-//        //    public string Color { get; set; }
-//        //    public int Speed { get; set; }
+        //CLASS:Sınıflar, OOP'de nesnelerin özelliklerini (alanlar) ve davranışlarını (metotlar) tanımlayan yapıdır. Örneğin, "Araba" sınıfı, renk ve hız gibi özelliklerle "HareketEt" gibi metotları içerebilir.
+        //class Car
+        //{
+        //    public string Color { get; set; }
+        //    public int Speed { get; set; }
 
-//        //    public void Accelerate()
-//        //    {
-//        //        Speed += 10;
-//        //    }
-//        //}
+        //    public void Accelerate()
+        //    {
+        //        Speed += 10;
+        //    }
+        //}
 
-//        //KAPSÜLLEME:Kapsülleme, bir nesnenin özelliklerini ve davranışlarını diğer nesnelerden gizleme işlemidir. Kapsülleme, nesnelerin daha tutarlı ve sağlam olmasını sağlar.
+        //KAPSÜLLEME:Kapsülleme, bir nesnenin özelliklerini ve davranışlarını diğer nesnelerden gizleme işlemidir. Kapsülleme, nesnelerin daha tutarlı ve sağlam olmasını sağlar.
 
-//        //Kapsülleme, public, protected, private ve internal erişim belirleyicilerini kullanarak yapılır. public erişim belirleyicisi, bir özelliğin veya davranışın tüm koddan erişilebilir olduğunu belirtir. protected erişim belirleyicisi, bir özelliğin veya davranışın aynı sınıftan türetilen sınıflardan erişilebilir olduğunu belirtir. private erişim belirleyicisi, bir özelliğin veya davranışın sadece aynı sınıftan erişilebilir olduğunu belirtir. internal erişim belirleyicisi, bir özelliğin veya davranışın aynı assembly'den erişilebilir olduğunu belirtir.
+        //Kapsülleme, public, protected, private ve internal erişim belirleyicilerini kullanarak yapılır. public erişim belirleyicisi, bir özelliğin veya davranışın tüm koddan erişilebilir olduğunu belirtir. protected erişim belirleyicisi, bir özelliğin veya davranışın aynı sınıftan türetilen sınıflardan erişilebilir olduğunu belirtir. private erişim belirleyicisi, bir özelliğin veya davranışın sadece aynı sınıftan erişilebilir olduğunu belirtir. internal erişim belirleyicisi, bir özelliğin veya davranışın aynı assembly'den erişilebilir olduğunu belirtir.
 
-//        //public class Person
-//        //{
-//        //    private string Name { get; set; }
-//        //    private int Age { get; set; }
-//        // }
-//        //Static:Nesne örneği alınmadan ulaşabildiğimiz metot, field, sınıf...DEĞİŞTİRİLEMEZ
-//        //public class Person
-//        //{
-//        //    public static int StaticAge { get; set; }
-//        //    public static void StaticSayHi()
-//        //    {
-//        //        Console.WriteLine("Hi!");
-//        //    }
-//        //}
-//        //INHERITANCE: Inheritance, bir sınıfın özelliklerini ve davranışlarını başka bir sınıfa aktarmasıdır.
-//        //public class SportsCar : Car
-//        //{
-//        //    public int Horsepower { get; set; }
+        public class Person
+        {
+            private string Name { get; set; }
+            private int Age { get; set; }
+        }
+        Static:Nesne örneği alınmadan ulaşabildiğimiz metot, field, sınıf...DEĞİŞTİRİLEMEZ
+        public class Person
+        {
+            public static int StaticAge { get; set; }
+            public static void StaticSayHi()
+            {
+                Console.WriteLine("Hi!");
+            }
+        }
+        //INHERITANCE: Inheritance, bir sınıfın özelliklerini ve davranışlarını başka bir sınıfa aktarmasıdır.
+        //public class SportsCar : Car
+        //{
+        //    public int Horsepower { get; set; }
 
-//        //    public void Accelerate()
-//        //    {
-//        //        // SportsCar'ın hızını Car'ın hızından 20 daha fazla artırır
-//        //        Speed += 10 + Horsepower;
-//        //    }
-//        //}
-//        //ENUM:C# programlama dilinde "enum" (enumeration), belirli bir veri türünü temsil eden ve genellikle sabit değerlerin sembolik adlarını içeren bir türdür.
-//        //enum DayOfWeek
-//        //{
-//        //    Monday,
-//        //    Tuesday,
-//        //    Wednesday,
-//        //    Thursday,
-//        //    Friday,
-//        //    Saturday,
-//        //    Sunday
-//        //}
-//        //ABSTRACT:Bir abstract sınıfın önemli özelliği, doğrudan bir nesne örneği (instance) oluşturulamamasıdır. Yani abstract sınıflardan doğrudan nesne oluşturulamaz, ancak abstract sınıfları temel alarak türetilen somut sınıflar oluşturulabilir.
-//        //abstract class Shape
-//        //{
-//        //    public string Color { get; set; }
+        //    public void Accelerate()
+        //    {
+        //        // SportsCar'ın hızını Car'ın hızından 20 daha fazla artırır
+        //        Speed += 10 + Horsepower;
+        //    }
+        //}
+        //ENUM:C# programlama dilinde "enum" (enumeration), belirli bir veri türünü temsil eden ve genellikle sabit değerlerin sembolik adlarını içeren bir türdür.
+        //enum DayOfWeek
+        //{
+        //    Monday,
+        //    Tuesday,
+        //    Wednesday,
+        //    Thursday,
+        //    Friday,
+        //    Saturday,
+        //    Sunday
+        //}
+        //ABSTRACT:Bir abstract sınıfın önemli özelliği, doğrudan bir nesne örneği (instance) oluşturulamamasıdır. Yani abstract sınıflardan doğrudan nesne oluşturulamaz, ancak abstract sınıfları temel alarak türetilen somut sınıflar oluşturulabilir.
+        //abstract class Shape
+        //{
+        //    public string Color { get; set; }
 
-//        //    public abstract double CalculateArea(); // Soyut metot
-//        //}
-//        //class Circle : Shape
-//        //{
-//        //    public double Radius { get; set; }
+        //    public abstract double CalculateArea(); // Soyut metot
+        //}
+        //class Circle : Shape
+        //{
+        //    public double Radius { get; set; }
 
-//        //    public override double CalculateArea()
-//        //    {
-//        //        return Math.PI * Radius * Radius;
-//        //    }
-//        //}
+        //    public override double CalculateArea()
+        //    {
+        //        return Math.PI * Radius * Radius;
+        //    }
+        //}
 
-//        //class Rectangle : Shape
-//        //{
-//        //    public double Width { get; set; }
-//        //    public double Height { get; set; }
+        //class Rectangle : Shape
+        //{
+        //    public double Width { get; set; }
+        //    public double Height { get; set; }
 
-//        //    public override double CalculateArea()
-//        //    {
-//        //        return Width * Height;
-//        //    }
-//        //}
+        //    public override double CalculateArea()
+        //    {
+        //        return Width * Height;
+        //    }
+        //}
 
-//        //INTERFACE:Interface, bir sınıfın sahip olması gereken özellikleri ve davranışlarını tanımlayan bir yapıdır. Interface'ler, nesneler oluşturmak için kullanılamaz. Interface'ler, inheritance ve polymorphism için kullanılı
-//        //public interface IAnimal
-//        //{
-//        //    void MakeSound();
-//        //    void Eat();
-//        //}
-//        //public class Dog : IAnimal
-//        //{
-//        //    public void MakeSound()
-//        //    {
-//        //        Console.WriteLine("Woof!");
-//        //    }
+        //INTERFACE:Interface, bir sınıfın sahip olması gereken özellikleri ve davranışlarını tanımlayan bir yapıdır. Interface'ler, nesneler oluşturmak için kullanılamaz. Interface'ler, inheritance ve polymorphism için kullanılı
+        //public interface IAnimal
+        //{
+        //    void MakeSound();
+        //    void Eat();
+        //}
+        //public class Dog : IAnimal
+        //{
+        //    public void MakeSound()
+        //    {
+        //        Console.WriteLine("Woof!");
+        //    }
 
-//        //    public void Eat()
-//        //    {
-//        //        Console.WriteLine("I'm eating!");
-//        //    }
-//        //}
-
-
+        //    public void Eat()
+        //    {
+        //        Console.WriteLine("I'm eating!");
+        //    }
+        //}
 
 
 
 
-//    }
 
-//}
+
+    }
+
+}
